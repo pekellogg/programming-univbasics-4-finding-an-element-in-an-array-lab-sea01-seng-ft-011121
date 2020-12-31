441d566e51812349de080ed676ff66856925f5c1
Peyton Kellogg <pekellogg@hotmail.com>
2020-12-31 04:19:12 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,8 +1,7 @@
 def find_element_index(array, value_to_find)
   count = 0
   while count < array.length
-  array.include?(value_to_find)
-  return value_to_find
+  puts array.include? == value_to_find
   count += 1
   end
 end
