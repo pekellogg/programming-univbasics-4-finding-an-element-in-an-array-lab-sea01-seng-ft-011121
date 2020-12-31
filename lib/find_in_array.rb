4ebdd9a0d6f71bbb66ba57eaf591532372024d75
Peyton Kellogg <pekellogg@hotmail.com>
2020-12-31 04:21:39 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,9 +3,9 @@ def find_element_index(array, value_to_find)
   found_value_index = nil
   while count < array.length
   if condition
-    
+
   end array[count] == value_to_find
-  
+
   count += 1
   end
 end
